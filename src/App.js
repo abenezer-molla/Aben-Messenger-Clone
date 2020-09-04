94f97870ac0a44949b2c1b0205a0352b5b172508
EbenWells <57775399+EbenWells@users.noreply.github.com>
2020-09-04 16:50:36 -0700

    update
    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Aben from './aben';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hello World</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/kidAben.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Kid extends Component {
+    state = {
+       
+      }
+
+
+
+  
+
+
+    render() { 
+        return ( 
+
+            <div>
+
+               
+                
+                Hello
+
+            </div>
+
+         );
+    }
+
+   
+
+
+}
+ 
+export default Kid;
